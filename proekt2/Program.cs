dae1a2d1da3d1237da580b3b68ebd96cb10c538a
Efim Malyy <she3442bdu@gmail.com>
2022-12-15 20:37:24 +0800

    pup

---
+++ proekt2/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// задача 8
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= a; i += 2)
+    if (a % 2 == 0)
+        Console.WriteLine(i);
