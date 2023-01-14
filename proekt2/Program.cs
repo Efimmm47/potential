0e7f60f7b4cf6c9478a4b3d0fb5c6f26767d9c41
Efim Malyy <she3442bdu@gmail.com>
2023-01-14 17:10:39 +0800

    domashka

--- proekt2/Program.cs
+++ proekt2/Program.cs
@@ -1,53 +1,67 @@
-﻿// Задача 19
-Console.WriteLine("введите пятизначное число которое вы хотите проверить на палиндром");
-string polin = Console.ReadLine();
-int kolvo = polin.Length;
-
-if (kolvo == 5) 
+﻿//задача 25
+void ToDegree(int a, int b)
 {
-    if (polin[0] == polin[4] && polin[1] == polin[3]) 
+    int result = 1;
+    for (int i = 1; i <= b; i++)
     {
-        Console.WriteLine(polin +"- число является палиндромом");
+        result = result * a;
     }
-    else
-    {
-        Console.WriteLine(polin +" - число НЕ является палиндромом");
-    }
+    Console.WriteLine(a + " в степени " + b + " = " + result);
 }
-else
+int ReadInt(string message)
 {
-    Console.WriteLine("Ошибка!!!");
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int numberA = ReadInt("Введите число: ");
+int numberB = ReadInt("Введите степень: ");
+ToDegree(numberA, numberB);
+
+//задача 27
+int number = ReadInt("Введите число: ");
+
+int len = NumberLen(number);
+SumNum(number, len);
+
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
 }
 
-//Задача 21
+void SumNum(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"сумма цифр {sum}");
+}
+
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
+
+//здача 29
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
 }
-int x1 = ReadInt("Введите координату X1 первой точки: ");
-int y1 = ReadInt("Введите координату Y1 первой точки: ");
-int z1 = ReadInt("Введите координату Z1 первой точки: ");
-int x2 = ReadInt("Введите координату X2 второй точки: ");
-int y2 = ReadInt("Введите координату Y2 второй точки: ");
-int z2 = ReadInt("Введите координату Z2 второй точки: ");
 
-int A = x2 - x1;
-int B = y2 - y1;
-int C = z1 - z2;
-
-double length = Math.Sqrt(A * A + B * B + C * C);
-Console.WriteLine($"Длинна отрезка {length}");
-
-//Задача 23
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
-}
-int number = ReadInt("Введите число N: ");
-
-for (int i = 1; i <= number; i++)
-{ 
-    Console.Write($"{i*i*i} ");
 }
